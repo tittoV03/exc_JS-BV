e1a2882ada67f9e34a2cd99c5d5e2723aad136ce
Brandon Villa <gogedresxenoverse87@gmail.com>
2026-08-29 23:07:07 -0500

    Claro, voy a mandar a ejecutar JS sin tener el HTML para testear los prompts, creé el archivo Index para adjuntarle el archivo JS

--- dateExc.js
+++ dateExc.js
@@ -2,3 +2,9 @@
 // Ingresar una fecha mediante un prompt (en formato mes-dia-año) y luego, mostrar
 // mediante un alert, a que día de la semana corresponde.
 
+let month = Number(prompt("Por favor ingrese el mes: "));
+
+while (isNaN(month) && month < 1 || month > 12)
+{
+    console.log("El mes ingresado no es valido, intentelo de nuevo.");
+}
