8e029dbc1618825400aaf0d3fccf555e85291a20
Brandon Villa <gogedresxenoverse87@gmail.com>
2026-08-29 23:07:07 -0500

    Agregue el comentario de lo que se trata el ejercicio de las fechas

---
+++ dateExc.js
@@ -0,0 +1,4 @@
+// Ejercicio #1 (aunque en realidad es el número 7 en base a mis notas de cuaderno)
+// Ingresar una fecha mediante un prompt (en formato mes-dia-año) y luego, mostrar
+// mediante un alert, a que día de la semana corresponde.
+
