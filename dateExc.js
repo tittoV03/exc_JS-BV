47cf3051a62c6ea1aafe15d63704c6b480633dc5
Brandon Villa <gogedresxenoverse87@gmail.com>
2026-08-29 23:07:08 -0500

    A la variable month le quite el null, re hice la función de modo que mientras sea true, inicie pidiendo el valor del mes, si este NO es NaN y además, se verifica mejor que la anterior vez que si este es igual o mayor a uno y menor o igual a 12, se detenga el ciclo, si no, se repite mientras console log indica que no es valido. Aunque si usara este mismo ciclo para día y mes, sería repetitivo, veré que otra forma se puede usar

--- dateExc.js
+++ dateExc.js
@@ -2,18 +2,14 @@
 // Ingresar una fecha mediante un prompt (en formato mes-dia-año) y luego, mostrar
 // mediante un alert, a que día de la semana corresponde.
 
-let month = null;
+let month;
 
 while (true)
 {
     month = Number(prompt("Por favor ingrese el mes: "));
 
-    if (month != null || !isNaN(month) && month > 1 || month < 12)
-    {
+    if (!isNaN(month) && month >= 1 && month <= 12)
         break;
-    }
-    else
-    {
-        console.log("El mes ingresado no es valido, intentelo de nuevo.")
-    }
+
+    console.log("El mes ingresado no es valido, intentelo de nuevo.")
 }
